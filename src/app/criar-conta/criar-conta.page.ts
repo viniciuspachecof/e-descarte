8f9aedf85f147f626e84db46bf84b9adbbd73cf0
Andrew Dutra Jorge <andrewdutra99@gmail.com>
2020-10-04 02:02:51 -0300

    cadastro de usuario

--- src/app/criar-conta/criar-conta.page.ts
+++ src/app/criar-conta/criar-conta.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController, LoadingController, NavController } from '@ionic/angular';
+import { Usuario } from 'src/app/models/Usuario.interface';
+import { UsuarioService } from 'src/app/services/usuario.service';
 
 @Component({
   selector: 'app-criar-conta',
@@ -7,9 +10,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CriarContaPage implements OnInit {
 
-  constructor() { }
+  usuario: Usuario;
+
+  constructor(
+    private alertController: AlertController,
+    private loadingController: LoadingController,
+    private usuarioService: UsuarioService,
+    private navController: NavController,
+  ) {
+    this.usuario = {
+      nome: null,
+      email: null,
+      senha: null,    
+    }
+   }
 
   ngOnInit() {
   }
 
+  async salvar() {
+    let dto = {
+        id: this.usuario.id,
+        nome: this.usuario.nome,
+        email: this.usuario.email,
+        senha: this.usuario.senha,
+      };
+
+    let loading = await this.loadingController.create({ message: 'Salvando' });
+    loading.present();
+
+    this.usuarioService
+      .salvar(dto)
+      .subscribe(() => {
+        loading.dismiss();
+        this.navController.navigateForward(['/login']);
+      }, () => {
+        loading.dismiss();
+        this.mensagemAlerta();
+      });
+  }
+
+  async mensagemAlerta() {
+    const alerta = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'Alerta',
+      message: 'Erro ao criar conta.',
+      buttons: ['OK']
+    });
+  }
 }
--- src/app/models/Usuario.interface.ts
+++ src/app/models/Usuario.interface.ts
@@ -1,6 +1,6 @@
 export interface Usuario {    
     id?: number;
     nome: string;
-    fone: string;
+    email: string;
     senha: string;
 };
